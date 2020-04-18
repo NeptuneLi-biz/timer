a7bf13f0a31a193a762d2becb7788ed75f15ea59
Neptune <neptune.li.biz@gmail.com>
2020-04-18 17:45:14 +0800

    v2

--- all.js
+++ all.js
@@ -4,11 +4,23 @@ const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
+const todayAdd3 = new Date();
 
-let countDown = new Date('2020/04/19 10:39:50').getTime();
+todayAdd3.setHours(todayAdd3.getHours() + 3);
+todayAdd3.getTime();
+
+let tempDate = Date.parse(JSON.parse(localStorage.getItem('timerTempData'))) || todayAdd3;
+
+// let tempDate = new Date('2020/04/19 10:39:50').getTime();
+
+// If first visit, save to localStorage.
+if (tempDate === todayAdd3) {
+  localStorage.setItem('timerTempData', JSON.stringify(tempDate));
+}
+
 x = setInterval(() => {    
   let now = new Date().getTime();
-  distance = countDown - now;
+  distance = tempDate - now;
   if (distance > 0 ) {
     document.getElementById('days').innerText = Math.floor(distance / (day));
     document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour));
